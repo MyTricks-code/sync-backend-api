1fd99320583eddcb187786d2d1ea1f63f1064bf0
Vishal Goswami <87699253+MyTricks-code@users.noreply.github.com>
2026-02-05 14:48:30 +0530

    mongoDB setup

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import 'dotenv/config'
+import connectDB from './config/mongoDB.js';
 
+// config
 const PORT = 8000 || process.env.PORT
 const app = express()
+connectDB()
+
+// Middlewares
+app.use(express.json());
 
 app.get("/", (req, res)=>{
     res.send("SYNC AIT BACKEND API")
