5f9660e5a4b18f9effdb0082f8a1072dec5816c3
Vishal Goswami <87699253+MyTricks-code@users.noreply.github.com>
2026-02-05 14:57:17 +0530

    created user Model

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 
 const connectDB = async ()=>{
-    await mongoose.connect(`${process.env.DB_URL}/sync`)
     mongoose.connection.on('connected', ()=>{
         console.log('database connected')
     })
+    await mongoose.connect(`${process.env.DB_URL}/sync`)
 }
 
 export default connectDB
